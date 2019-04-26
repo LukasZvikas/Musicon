cf97b195d59addf7bce4ae684ca282ac4da098d9
LukasZvikas <lzvikas1@gmail.com>
2019-04-26 15:08:09 -0400

    Feature: configured video player to load new song preview when song changes

--- src/pages/dashboard/card/Card.tsx
+++ src/pages/dashboard/card/Card.tsx
@@ -3,7 +3,12 @@ import "./Card.css";
 import { BackArrow } from "../../../svg/backArrow";
 import { NextArrow } from "../../../svg/nextArrow";
 
-export const Card = () => {
+interface CardProps {
+  nextSong: () => void;
+  previousSong: () => void;
+}
+
+export const Card = ({ nextSong, previousSong }: CardProps) => {
   const [rotate, rotateCard] = useState(false);
 
   const changeCard = () => rotateCard(!rotate);
@@ -14,6 +19,7 @@ export const Card = () => {
         action={() => {
           console.log("clicked");
           changeCard();
+          previousSong();
         }}
       />
       <div className="swipe-card__image-wrapper mx-4">
@@ -24,7 +30,9 @@ export const Card = () => {
           src="https://i.scdn.co/image/db2133234d458f432ca207e163a801524a15f993"
         />
         <img
-          className={`swipe-card__image swipe-card__image-back ${rotate ? "rotateBack" : ""}`}
+          className={`swipe-card__image swipe-card__image-back ${
+            rotate ? "rotateBack" : ""
+          }`}
           src="https://i.scdn.co/image/cdca7dc20c778ada42fb18506ea1f26857f01d17"
         />
       </div>
@@ -33,6 +41,7 @@ export const Card = () => {
         action={() => {
           console.log("clicked");
           changeCard();
+          nextSong();
         }}
       />
     </div>
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "./card/Card";
 import { Button } from "../../components/button";
 import "../../Shared.css";
 import "./Dashboard.css";
 
+const songData = [
+  {
+    songName: "ZEZE",
+    artists: ["Kodak Black", "Offset", "Travis Scott"],
+    preview:
+      "https://p.scdn.co/mp3-preview/6ccc75509be729ffb01b1df3a8183e4e88dc6303?cid=774b29d4f13844c495f206cafdad9c86"
+  },
+  {
+    songName: "SICKO MODE",
+    artists: ["Travis Scott", "Drake"],
+    preview:
+      "https://p.scdn.co/mp3-preview/1c3c01c64477b356e28a652b6447f4ef96689a71?cid=774b29d4f13844c495f206cafdad9c86"
+  }
+];
+
 const Dashboard = () => {
+  const [songs] = useState(songData);
+  const [currentIndex, updateIndex] = useState(0);
+
+  const nextSong = () => {
+    updateIndex(currentIndex + 1);
+    document.querySelector(".dashboard__video").load();
+  };
+
+  const previousSong = () => {
+    updateIndex(currentIndex - 1);
+    document.querySelector(".dashboard__video").load();
+  };
+
+  const renderArtists = arr => {
+    return arr.map((artist, index) => {
+      if (index === arr.length - 1) return artist;
+      return artist + ", ";
+    });
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center h-100 flex-column">
-      <Card />
+      <Card nextSong={() => nextSong()} previousSong={() => previousSong()} />
       <div className="dashboard__heading-wrapper mb-2 mt-2">
-        <div className="heading__primary m-1 text-center">ZEZE</div>
-        <div className="heading__secondary mt-2 text-white">
-          Kodak Black, Offset, Travis Scott
+        <div className="heading__primary m-1 text-center">
+          {songs[currentIndex].songName}
         </div>
-        <video className="dashboard__video" controls autoplay name="media">
-          <source
-            src="https://p.scdn.co/mp3-preview/6ccc75509be729ffb01b1df3a8183e4e88dc6303?cid=774b29d4f13844c495f206cafdad9c86"
-            type="audio/mpeg"
-          />
+        <div className="heading__secondary mt-2 text-white">
+          {renderArtists(songs[currentIndex].artists)}
+        </div>
+        <video className="dashboard__video" controls name="media">
+          <source src={`${songs[currentIndex].preview}`} type="audio/mpeg" />
         </video>
       </div>
 
