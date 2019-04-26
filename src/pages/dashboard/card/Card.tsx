d2f82dc653b94d2c52402d1424a8cd316d64319f
LukasZvikas <lzvikas1@gmail.com>
2019-04-26 14:27:19 -0400

    Feature: added card rotation animation

--- src/pages/dashboard/card/Card.tsx
+++ src/pages/dashboard/card/Card.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
-import { NextArrow } from "../../../svg/nextArrow";
 import { BackArrow } from "../../../svg/backArrow";
+import { NextArrow } from "../../../svg/nextArrow";
 
 export const Card = () => {
+  const [rotate, rotateCard] = useState(false);
+
+  const changeCard = () => rotateCard(!rotate);
+
   return (
     <div className="swipe-card d-flex align-items-center">
-      <BackArrow />
+      <BackArrow
+        action={() => {
+          console.log("clicked");
+          changeCard();
+        }}
+      />
       <div className="swipe-card__image-wrapper mx-4">
         <img
-          className="swipe-card__image"
+          className={`swipe-card__image swipe-card__image-front ${
+            rotate ? "rotateNext" : ""
+          }`}
           src="https://i.scdn.co/image/db2133234d458f432ca207e163a801524a15f993"
         />
+        <img
+          className={`swipe-card__image swipe-card__image-back ${rotate ? "rotateBack" : ""}`}
+          src="https://i.scdn.co/image/cdca7dc20c778ada42fb18506ea1f26857f01d17"
+        />
       </div>
-      <NextArrow />
+
+      <NextArrow
+        action={() => {
+          console.log("clicked");
+          changeCard();
+        }}
+      />
     </div>
   );
 };
--- src/svg/nextArrow.tsx
+++ src/svg/nextArrow.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export const NextArrow = () => {
+interface FrontArrowProps {
+  action: () => void;
+}
+
+export const NextArrow = ({ action }: FrontArrowProps) => {
   return (
-    <div>
+    <div onClick={action}>
       <svg version="1.1" viewBox="0 0 240.823 240.823" height="40" width="40">
         <path
           fill="#fff"
-          id="Chevron_Right_1_"
           d="M183.189,111.816L74.892,3.555c-4.752-4.74-12.451-4.74-17.215,0c-4.752,4.74-4.752,12.439,0,17.179
        l99.707,99.671l-99.695,99.671c-4.752,4.74-4.752,12.439,0,17.191c4.752,4.74,12.463,4.74,17.215,0l108.297-108.261
        C187.881,124.315,187.881,116.495,183.189,111.816z"
