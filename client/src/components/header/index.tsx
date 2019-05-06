fd167e6801095f7fe2eb21c10c35c2366d433f01
LukasZvikas <lzvikas1@gmail.com>
2019-05-05 23:32:40 -0400

    updated logo

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -14,12 +14,15 @@ const Header = () => {
 
   return (
     <div className="header px-4">
-      <div className=" header__logo bg-transparent d-flex align-items-center">
-        <LogoIcon fill={getPrimaryColor()} />
-        <div className="header__title bg-transparent ml-2">
-          <Link to="/">Musicon</Link>
+      <Link to="/">
+        <div className="header__logo d-flex bg-transparent">
+          <LogoIcon fill={getPrimaryColor()} />
+          <div className="d-flex align-items-center ml-2 bg-transparent">
+            Musicon
+          </div>
         </div>
-      </div>
+      </Link>
+
       <ul className="header__links d-flex bg-transparent align-items-center">
         <li className="d-flex bg-transparent align-items-center mr-4">
           <Link to="/explore">Explore</Link>
